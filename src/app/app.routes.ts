458dd84bb1af5f63b6954e8008a3b232494dc533
Tu nombre <anamariabmaza@gmail.com>
2026-06-11 15:57:13 -0300

    fix: address code review feedback for create project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { CreateProjectComponent } from './components/create-project/create-projects.component';
-
+import { CreateProjectComponent } from './components/create-project/create-project.component';
 export const routes: Routes = [
   { path: 'proyectos/crear', component: CreateProjectComponent },
   { path: '', redirectTo: 'proyectos/crear', pathMatch: 'full' }
