b42dafe2bbdadc45280dd8758f5c98d3c2e07099
Tu nombre <anamariabmaza@gmail.com>
2026-06-13 16:16:46 -0300

    fix: eliminar archivos de otras features y limpiar app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,10 @@
 import { Routes } from '@angular/router';
 import { CreateProjectComponent } from './components/create-project/create-project.component';
-import { ProjectSummaryComponent } from './components/project-summary/project-summary.component';
-import { CreateTaskComponent } from './components/create-task/create-task.component';
 
 export const routes: Routes = [
   {
     path: 'proyectos/crear',
     component: CreateProjectComponent
-  },
-  {
-    path: 'project/:projectId/summary',
-    component: ProjectSummaryComponent
-  },
-  {
-    path: 'projects/:projectId/tasks/create',
-    component: CreateTaskComponent
   },
   {
     path: '',
